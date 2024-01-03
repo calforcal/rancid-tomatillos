1489eaf27500f697e054775bb4ae97b45d33713a
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-01-03 10:55:16 -0700

    Add NavButtons to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import movieData from './movieData'
+import NavButton from './NavButton/NavButton'
 
 function App() {
+  const [selectedButton, setSelectedButton] = useState()
+
+  function handleSelect(selectedButton) {
+    setSelectedButton(selectedButton);
+  }
+
   const movies = movieData.movies
   return (
     <>
+      <NavButton isSelected={selectedButton === "all"} onSelect={()=>handleSelect('all')}>All Movies</NavButton>
+      <NavButton isSelected={selectedButton === "favorites"} onSelect={()=>handleSelect('favorites')}>Favorites</NavButton>
+      <NavButton isSelected={selectedButton === "top-picks"} onSelect={()=>handleSelect('top-picks')}>Top Picks</NavButton>
+      <NavButton isSelected={selectedButton === "new"} onSelect={()=>handleSelect('new')}>New</NavButton>
       {movies.map((movie) => (
         <h1>{movie.title}</h1>
       ))}
