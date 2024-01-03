d4f66da2e8d69bf47000db51567ca3e181e3ede3
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-01-03 12:36:59 -0700

    Add components to main App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import movieData from './movieData'
 import NavButton from './NavButton/NavButton'
+import MovieCard from './MovieCard/MovieCard'
 
 function App() {
   const [selectedButton, setSelectedButton] = useState()
@@ -12,15 +13,17 @@ function App() {
 
   const movies = movieData.movies
   return (
-    <>
+    <div className='main'>
       <NavButton isSelected={selectedButton === "all"} onSelect={()=>handleSelect('all')}>All Movies</NavButton>
       <NavButton isSelected={selectedButton === "favorites"} onSelect={()=>handleSelect('favorites')}>Favorites</NavButton>
       <NavButton isSelected={selectedButton === "top-picks"} onSelect={()=>handleSelect('top-picks')}>Top Picks</NavButton>
       <NavButton isSelected={selectedButton === "new"} onSelect={()=>handleSelect('new')}>New</NavButton>
-      {movies.map((movie) => (
-        <h1>{movie.title}</h1>
-      ))}
-    </>
+      <div className='movie-cards-container'>
+        {movies.map((movie) => (
+          <MovieCard {...movie} />
+        ))}
+      </div>
+    </div>
   );
 }
 
