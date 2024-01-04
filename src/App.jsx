4be4e84851fe57fabd2bbd82a255aa790eb65be6
Michael Callahan <118235615+calforcal@users.noreply.github.com>
2024-01-04 15:55:25 -0700

    Add clickability to movie images for displaying full movie data

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,15 @@ import './App.css'
 import movieData from './movieData'
 import NavButton from './NavButton/NavButton'
 import MovieCard from './MovieCard/MovieCard'
+import MovieDetailsCard from './MovieDetailsCard/MovieDetailsCard'
 
 function App() {
   const [selectedButton, setSelectedButton] = useState()
+  const [selectedMovie, setSelectedMovie] = useState()
+
+  function handleClickMovie(movie) {
+    setSelectedMovie(movie);
+  }
 
   function handleSelect(selectedButton) {
     setSelectedButton(selectedButton);
@@ -18,11 +24,14 @@ function App() {
       <NavButton isSelected={selectedButton === "favorites"} onSelect={()=>handleSelect('favorites')}>Favorites</NavButton>
       <NavButton isSelected={selectedButton === "top-picks"} onSelect={()=>handleSelect('top-picks')}>Top Picks</NavButton>
       <NavButton isSelected={selectedButton === "new"} onSelect={()=>handleSelect('new')}>New</NavButton>
-      <div className='movie-cards-container'>
-        {movies.map((movie) => (
-          <MovieCard {...movie} />
-        ))}
-      </div>
+      {selectedMovie == null
+      ? <div className='movie-cards-container'>
+          {movies.map((movie) => (
+            <MovieCard key={movie.id} {...movie} onClick={() => {handleClickMovie(movie)}} />
+          ))}
+        </div>
+      : <MovieDetailsCard {...selectedMovie} onClick={() => {handleClickMovie()}}/>
+      }
     </div>
   );
 }
